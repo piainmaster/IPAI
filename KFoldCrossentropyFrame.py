a38e0acbb157ae72edfa0e0f273a5b481f99f112
Noora <zeinab-noora.mohammadi@prime-force.com>
2023-11-26 20:58:37 +0100

    entropy of each frame

---
+++ KFoldCrossentropyFrame.py
@@ -0,0 +1,81 @@
+ #incase with exact folds
+
+import cv2
+import os
+import numpy as np
+from sklearn.model_selection import LeaveOneOut, StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from skimage.measure import shannon_entropy
+
+def calculate_entropies(image_path, num_frames):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+    # Divide the image into frames
+    entropies = []
+    height, width = image.shape
+
+    frame_size = width // num_frames
+
+    for i in range(num_frames):
+        start_x = i * frame_size
+        end_x = (i + 1) * frame_size
+        frame = image[:, start_x:end_x]
+        entropy = shannon_entropy(frame)
+        entropies.append(entropy)
+
+    return entropies
+
+
+def load_images(folder_path, num_frames):
+    images = []
+    labels = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            image_path = os.path.join(folder_path, filename)
+            entropies = calculate_entropies(image_path, num_frames)
+            images.append(entropies)
+            labels.append(folder_path)
+    return np.array(images), np.array(labels)
+
+
+def main():
+    spoof_folder = "E:\Data_prepared\PLUS\spoofed"
+    original_folder = "E:\Data_prepared\PLUS\genuine"
+    num_frames = 6  # Adjust the number of frames as needed
+
+    spoof_images, spoof_labels = load_images(spoof_folder, num_frames)
+    original_images, original_labels = load_images(original_folder, num_frames)
+
+    spoof_labels = np.ones(len(spoof_images))  # Spoof class label
+    original_labels = np.zeros(len(original_images))  # Original class label
+
+    images = np.vstack((spoof_images, original_images))
+    labels = np.concatenate((spoof_labels, original_labels), axis=0)
+
+    skf = StratifiedKFold(n_splits=5)
+
+    correct_predictions = 0
+
+    for train_index, test_index in skf.split(images, labels):
+        X_train, X_test = images[train_index], images[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+
+        # KNN classifier
+        k = 3  # You can adjust the number of neighbors as needed
+        knn_classifier = KNeighborsClassifier(n_neighbors=k)
+        knn_classifier.fit(X_train, y_train)
+
+        # Test image
+        test_image_path = "E:\Data_prepared\IDIAP\genuine\mest.png"
+        test_entropies = calculate_entropies(test_image_path, num_frames)
+        test_label = knn_classifier.predict([test_entropies])
+
+        if test_label == y_test[0]:
+            correct_predictions += 1
+
+    accuracy = correct_predictions / len(images)
+    print(f"Accuracy with 5-fold Cross-Validation: {accuracy * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    main()
